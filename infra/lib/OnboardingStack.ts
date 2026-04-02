110b8bc919f9d7e593e3ba976f5f8bb8b47d0161
AdannaAkwats <adanna.akwats@hotmail.com>
2026-04-02 19:08:39 +0200

    fix: add more perms + sec to cdk infra

--- infra/lib/OnboardingStack.ts
+++ infra/lib/OnboardingStack.ts
@@ -4,18 +4,26 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as kms from 'aws-cdk-lib/aws-kms';
 
 export class OnboardingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const onboardedBucketKey = new kms.Key(this, 'OnboardedBooksBucketKey', {
+      enableKeyRotation: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE,
+    });
+
     const onboardedBucket = new s3.Bucket(this, 'OnboardedBooksBucket', {
       bucketName: `onboardedbooks-${cdk.Aws.ACCOUNT_ID}`,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      encryption: s3.BucketEncryption.S3_MANAGED,
+      encryption: s3.BucketEncryption.KMS,
+      encryptionKey: onboardedBucketKey,
       versioned: true,
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      removalPolicy: cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE,
       autoDeleteObjects: false,
+      enforceSSL: true,
     });
 
     const booksMetadataTable = new dynamodb.Table(this, 'BooksMetadataTable', {
@@ -25,11 +33,13 @@ export class OnboardingStack extends cdk.Stack {
         type: dynamodb.AttributeType.STRING,
       },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      pointInTimeRecovery: true,
+      encryption: dynamodb.TableEncryption.AWS_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE,
     });
 
     const onboardFunction = new lambda.Function(this, 'OnboardFunction', {
-      runtime: lambda.Runtime.NODEJS_18_X,
+      runtime: lambda.Runtime.NODEJS_24_X,
       handler: 'onboard/index.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../../backend/dist')),
       memorySize: 3008,
@@ -39,7 +49,9 @@ export class OnboardingStack extends cdk.Stack {
       },
     });
 
-    onboardedBucket.grantRead(onboardFunction);
+    onboardedBucket.grantReadWrite(onboardFunction);
+    onboardedBucketKey.grantEncryptDecrypt(onboardFunction);
+    booksMetadataTable.grantReadWriteData(onboardFunction);
 
     new cdk.CfnOutput(this, 'OnboardingBucketName', {
       value: onboardedBucket.bucketName,
